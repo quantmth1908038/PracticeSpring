01957bde34d201bf53b9ccec491bb95849b17c2f
quantmth1908038 <54584382+quantmth1908038@users.noreply.github.com>
2021-05-22 14:46:49 +0700

    commit

---
+++ src/main/java/com/example/truongminhquan/TruongminhquanApplication.java
@@ -0,0 +1,13 @@
+package com.example.truongminhquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruongminhquanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruongminhquanApplication.class, args);
+    }
+
+}
